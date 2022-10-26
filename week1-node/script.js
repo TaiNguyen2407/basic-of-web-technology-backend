40c92c8869643c0f902d7260d110486987fc6086
Tai Nguyen <phatn@metropolia.fi>
2022-10-26 11:28:04 +0300

    week 1 exercise 1 & 2 done

--- week1-node/script.js
+++ week1-node/script.js
@@ -1,1 +1,19 @@
+'use strict';
+
+const _ = require('lodash')
+
+//Never uyse var
+//const or let is preferred
+
+
+_.camelCase
+
+//Ex 1
 console.log("This is JavaScript");
+console.log("This course is interesting");
+console.log("Testing");
+
+//Ex 2
+const output = 'testing nodemon, using lodash to convert this camel case';
+console.log(output);
+console.log(_.camelCase(output));
