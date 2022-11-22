8799f824e9bf23333a127fcbbae5ca0953f2f1f6
Tai Nguyen <phatn@metropolia.fi>
2022-11-22 14:20:39 +0200

    added authentication with global variable loggedIn

--- week4-state-starter/week4-state-starter/app.js
+++ week4-state-starter/week4-state-starter/app.js
@@ -1,16 +1,54 @@
 'use strict';
 const express = require('express');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const app = express();
 const port = 3000;
+
+
+const dummyUser = {
+  username : 'foo',
+  password : 'bar'
+};
+
+let loggedIn = true;
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true})); //Form data
+// app.use(session());
 
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/form', (req, res) => {
+  res.render('form');
+});
+
+app.get('/secret', (req, res) => {
+  if (loggedIn) {
+    res.render('secret');
+  } else {
+    res.redirect('/form');
+  }
+
+});
+
+app.post('/login', (req, res) => {
+  // check for username and password match
+  console.log(req.body);
+  if (req.body.username == dummyUser.username && req.body.password == dummyUser.password) {
+    loggedIn = true;
+    res.redirect('/secret');
+  } else {
+    res.redirect('/form');
+    // res.('Wrong username or password');
+  }
+  
 });
 
 app.get('/getCookie/', (req, res) => {
@@ -29,8 +67,5 @@ app.get('/setCookie/:color', (req, res) => {
   res.cookie('color', req.params.color);
   res.send('cookies set');
 });
-
-
-app.delete
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
