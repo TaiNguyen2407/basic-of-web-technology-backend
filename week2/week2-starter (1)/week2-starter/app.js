3f6ec1787e2662cbe14d6ab938b0d6452cc18683
Tai Nguyen <phatn@metropolia.fi>
2022-11-29 15:34:05 +0200

    added thumbnails and a part of exif-data

--- week2/week2-starter (1)/week2-starter/app.js
+++ week2/week2-starter (1)/week2-starter/app.js
@@ -7,16 +7,19 @@ const userRouter = require('./routes/userRoute');
 const authRouter = require('./routes/authRoute');
 const passport = require('./utils/passport');
 
+
 const port = 3000;
 
 //serve uploaded files
 app.use(express.static('uploads'));
+app.use('/thumbnails', express.static('thumbnails'));
 
 
 app.use(cors());
 app.use(express.json()); //for parsing application/json
 app.use(express.urlencoded({ extended: true })) //For parsing application/x-www-form-urlencoded
 app.use(passport.initialize());
+
 
 app.use('/auth', authRouter);
 app.use('/cat', passport.authenticate('jwt', {session: false}), catRouter);
